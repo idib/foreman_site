492882a199b108f3b2523573f15e12a3048f7749
arseniy96 <arsenzhar@gmail.com>
2016-04-21 17:21:05 +0300

    Add comments controller

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,7 @@ Rails.application.routes.draw do
 
   root to: 'home#index'
 
-  get 'home/index'
-
+  resources :comments
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
