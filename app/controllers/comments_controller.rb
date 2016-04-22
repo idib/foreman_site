2a74be0e3ef05249eecb30e007d157cc20af0900
arseniy96 <arsenzhar@gmail.com>
2016-04-22 20:03:04 +0300

    Add actions to comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,4 +2,23 @@ class CommentsController < ApplicationController
   def index
     @comments = Comment.all
   end
+
+  def new
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
 end
