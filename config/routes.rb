ff4144511f8b545669894e16306462e8c7662adc
arseniy96 <arsenzhar@gmail.com>
2016-04-23 17:46:31 +0300

    Install gem activeadmin

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   get 'about' => 'pages#about'
   get 'gallery' => 'pages#gallery'
   get 'price' => 'pages#price'
